89a36fbfccf1ab770bd142d05371a63d44454f79
jiyoung <one69jy@gmail.com>
2021-10-10 18:58:00 +0900

    JS : features section scroll-animation

--- js/main.js
+++ js/main.js
@@ -1,11 +1,19 @@
 $(function(){
   let $device = $('.device');
-  let $offset = 300;
-  let $deviceOffsetTop = $device.offset().top - $offset;
+  let $deviceOffsetTop = $device.offset().top - 400;
+
+  let $features = $('.features');
+  let $featuresOffsetTop = $features.offset().top - 400;
 
   $(window).scroll(function(){
+    // device section animation
     if($(this).scrollTop() > $deviceOffsetTop){
       $device.find('img').addClass('animate');
+    }
+    // features section animation
+    if($(this).scrollTop() > $featuresOffsetTop){
+      $features.find('img').addClass('grow-img');
+      $features.find('h4').addClass('animate');
     }
   });
 });
